664a38dbbe70cac86b0bbd498cab49e612d2bc19
DevGaikwad <141038667+DevGaikwad@users.noreply.github.com>
2023-12-12 13:42:15 +0530

    Add files via upload

---
+++ while.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+
+    // code no:1
+
+    // int b = 1;
+    // while (b <= 10)
+
+    // {
+    //     printf("hello \n");
+    //     b++;
+    // }
+
+    // code no:2
+
+    //  int i= 1;
+    //  while(i<=100)
+    //  {
+
+    //  if(i % 2 == 0){
+    //      printf("Even number is = %d\n",i);
+
+    //  }else
+    //  {
+    //     printf("Odd  number is = %d\n",i);
+    //  }
+    //  i++;
+    //  }
+
+    // code no:3
+
+    int a = 1;
+    while (a <= 100)
+    {
+        if (a % 2 != 0)
+        {
+            printf("%d\n", a);
+        }
+        a++;
+    }
+
+    return 0;
+}
